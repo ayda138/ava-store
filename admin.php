55783b295c0f8a102b848a6aaad4a18ceefb30ba
pariya <pari@xample.com>
2025-06-02 08:15:00 -0700

    ava

---
+++ admin.php
@@ -0,0 +1,8 @@
+<p>پنل ادمین</p>
+<header class="admin-panel">
+        <nav>
+            <ul>
+                <li><a href="mahsoolat_admin.php">محصولات</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ theme-header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+  <meta charset="UTF-8">
+  <title>ava store</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body dir="rtl">
+    <header>
+    <h1><i class="fas fa-store"></i>فروشگاه آوا</h1>
+    <nav>
+            <ul>
+                <li><i class="fa-solid fa-user"></i><a href="vorood.php">ورود</a></li>
+                <li><i class="fa-solid fa-right-to-bracket"></i><a href="sabt.php">ثبت نام</a></li>
+                <li><i class="fa-solid fa-right-to-bracket"></i><a href="mahsoolat.php">محصولات</a></li>
+                <li><a href="tamas.html">تماس با ما</a></li>
+            </ul>
+        </nav>
+  </header>
+    <main>
